6e16d5dcdd8ea328023903c29fad6a21705c7004
Andrey Mikhailichenko <andrey@mikhailichenko.name>
2019-10-13 20:47:15 +0300

    Resizers appear

--- src/core/EditableGroup.ts
+++ src/core/EditableGroup.ts
@@ -1,4 +1,4 @@
-import { Group, Box3, WireframeGeometry, BoxGeometry, LineSegments, Object3D } from "three";
+import { Group, Box3, WireframeGeometry, BoxGeometry, LineSegments, Object3D, Mesh, SphereGeometry, DoubleSide, MeshPhongMaterial, Vector3, Color } from "three";
 import { getGroupGeometry } from "../tools/helpers";
 
 export enum EditableGroupState {
@@ -9,6 +9,12 @@ export enum EditableGroupState {
 export class EditableGroup extends Group {
 
     private _selectFrame: LineSegments;
+    private _resizers: {
+        tl: Mesh,
+        tr: Mesh,
+        bl: Mesh,
+        br: Mesh,
+    }
     private _state = EditableGroupState.show;
     public isEditableGroup = true;
 
@@ -30,7 +36,13 @@ export class EditableGroup extends Group {
 
     private _updateFrame() {
 
-        if (this._selectFrame) super.remove(this._selectFrame);
+        if (this._selectFrame) {
+            super.remove(this._selectFrame);
+            super.remove(this._resizers.tl);
+            super.remove(this._resizers.tr);
+            super.remove(this._resizers.bl);
+            super.remove(this._resizers.br);
+        }
         if (this._state == EditableGroupState.editor) {
             const box = new Box3().setFromObject(this);
 
@@ -46,7 +58,40 @@ export class EditableGroup extends Group {
             this._selectFrame.position.z = (box.max.z - box.min.z) / 2;
 
             super.add(this._selectFrame);
+            this._createResizers(box);
         }
+    }
+
+    private _createResizers(box: Box3) {
+        const z = 0;
+        const w = box.max.x - box.min.x;
+        const h = box.max.y - box.min.y;
+        const diameter = 10;
+        this._resizers = {
+            tl: this._createSphere(new Vector3(-w/2, h/2, z), diameter, '0x55dd77'),
+            tr: this._createSphere(new Vector3(w/2, h/2, z), diameter, '0x55dd77'),
+            bl: this._createSphere(new Vector3(-w/2, -h/2, z), diameter, '0x55dd77'),
+            br: this._createSphere(new Vector3(w/2, -h/2, z), diameter, '0x55dd77'),
+        };
+        super.add(this._resizers.tl);
+        super.add(this._resizers.tr);
+        super.add(this._resizers.bl);
+        super.add(this._resizers.br);
+    }
+
+    private _createSphere(position, size, color, texture?){
+        var g = new SphereGeometry(size, 32, 32);
+        var maretialConf = { color: color }
+        if (texture) {
+            maretialConf['map'] = texture;
+            maretialConf['side'] = DoubleSide;
+        }
+        var material = new MeshPhongMaterial(maretialConf);
+        var sphere = new Mesh(g, material);
+        sphere.position.x = position.x;
+        sphere.position.y = position.y;
+        sphere.position.z = position.z;
+        return sphere;
     }
 
 }
