1fd68feaf0d99467513233f576f03e73757dac8f
Andrey <andrey@mikhailichenko.su>
2020-01-28 15:40:41 +0300

    FTP settings and deploy script

---
+++ deploy.js
@@ -0,0 +1,20 @@
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+const settings = require("./deploy.conf").getFtpParams();
+console.log({ settings });
+const config = {
+  user: settings.username,
+  password: settings.password,
+  host: settings.host,
+  port: settings.port,
+  localRoot: __dirname + "/" + settings.localRoot,
+  remoteRoot: settings.remoteRoot,
+  include: ["*", "**/*"],
+  deleteRemote: false,
+  forcePasv: true
+};
+
+ftpDeploy
+  .deploy(config)
+  .then(res => console.log("finished:", res))
+  .catch(err => console.log(err));
